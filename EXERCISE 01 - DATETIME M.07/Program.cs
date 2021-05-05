59e0f119b0a17550414507610fe91542d4b90b3c
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-05 09:21:51 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 01 - DATETIME M.07/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace EXERCISE_01___DATETIME_M._07 {
+    /// -------> START / MAIN 
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            DateTime d1 = new DateTime(2018, 11, 25);
+            DateTime d2 = new DateTime(2018, 11, 25, 20, 45, 3);
+            DateTime d3 = new DateTime(2018, 11, 25, 20, 45, 3, 500);
+            DateTime d4 = DateTime.Now;
+            DateTime d5 = DateTime.Today;
+            DateTime d6 = DateTime.Parse("2000-08-15");
+            DateTime d7 = DateTime.Parse("2000-08-15 13:05:58");
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+            Console.WriteLine(d5);
+            Console.WriteLine(d6);
+            Console.WriteLine(d7);
+
+            DateTime d8 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd");
+            DateTime d9 = DateTime.ParseExact("15/08/200 13:05:58", "dd/MM/yyyy HH:mm:ss");
+        }
+    }
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    /// -------> END
+}
